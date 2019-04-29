bccb70fd72ced36a348c728ca8b8e7e7b9a9f37c
Beichen Liang <liangbeichen@hotmail.com>
2019-04-29 15:28:18 -0700

    Add main

--- app/src/main/java/edu/us/ischool/lbc546/tipcalc/MainActivity.kt
+++ app/src/main/java/edu/us/ischool/lbc546/tipcalc/MainActivity.kt
@@ -2,11 +2,60 @@ package edu.us.ischool.lbc546.tipcalc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import android.text.TextWatcher
+import android.text.Editable
+import java.text.NumberFormat
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val charge = findViewById<EditText>(R.id.charge)
+        val tip = findViewById<Button>(R.id.tip)
+        tip.isEnabled = false
+
+        fun chargeStringToDouble(amount: String): Double {
+            val remove = Regex("[\$,.]")
+            val cleanString: String = remove.replace(amount, "")
+            return cleanString.toDouble()
+        }
+
+        // https://stackoverflow.com/questions/5107901/better-way-to-format-currency-input-edittext
+        charge.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.toString().equals("")) {
+                    charge.removeTextChangedListener(this)
+                    val parsed = chargeStringToDouble(charge.text.toString())
+                    val formatted: String = NumberFormat.getCurrencyInstance().format((parsed / 100))
+                    charge.setText(formatted)
+                    charge.setSelection(formatted.length)
+
+                    charge.addTextChangedListener(this)
+                }
+                tip.isEnabled = true
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
+        tip.setOnClickListener {
+            var currCharge = chargeStringToDouble(charge.text.toString()) / 100
+            val a = currCharge * 15 / 100
+            val tipToast: Toast = Toast.makeText(applicationContext, "$" + "%.2f".format(a), Toast.LENGTH_LONG)
+            tipToast.show()
+        }
+    }
+
+    companion object {
+        const val TAG = "MainActivity"
     }
 }
